c17bbcc2847721513fdc47677a19518e7ad72eb2
ericyael <eric3190@hotmail.com>
2021-09-16 19:09:30 -0500

    Primercomit

---
+++ funcion.js
@@ -0,0 +1,19 @@
+function resultado() {
+
+ 
+<!-- 1ª pregunta -->
+if (document.getElementById('p11').checked==true) {p1=1}
+else {p1=0}
+<!-- 2ª pregunta -->
+if (document.getElementById('p22').checked==true) {p2=1}
+else {p2=0}
+<!-- 3ª pregunta -->
+if (document.getElementById('p33').checked==true) {p3=1}
+else {p3=0}
+<!-- 4ª pregunta -->
+if (document.getElementById('p44').checked==true) {p4=1}
+else {p4=0}
+<!-- 5ª pregunta -->
+if (document.getElementById('p22').checked==true) {p5=1}
+alert("Gracias por hacer el quiz :)");
+}
